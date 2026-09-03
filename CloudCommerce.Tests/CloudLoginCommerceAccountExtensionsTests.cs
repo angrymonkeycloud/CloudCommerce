f222f33de856588c1f162140360578585fcabd9f
Elie Tebchrani <elietebchrani@angrymonkeyagency.com>
2026-09-03 11:26:53 +0300

    Cloud Login extension removed

--- CloudCommerce.Tests/CloudLoginCommerceAccountExtensionsTests.cs
+++ CloudCommerce.Tests/CloudLoginCommerceAccountExtensionsTests.cs
@@ -1,30 +1,30 @@
-using AngryMonkey.CloudCommerce;
-using AngryMonkey.CloudLogin;
+//using AngryMonkey.CloudCommerce;
+//using AngryMonkey.CloudLogin;
 
-namespace CloudCommerce.Tests;
+//namespace CloudCommerce.Tests;
 
-public class CloudLoginCommerceAccountExtensionsTests
-{
-    [Fact]
-    public void ToCommerceAccount_maps_user_identifier()
-    {
-        Guid userId = Guid.NewGuid();
-        UserModel user = new() { ID = userId };
+//public class CloudLoginCommerceAccountExtensionsTests
+//{
+//    [Fact]
+//    public void ToCommerceAccount_maps_user_identifier()
+//    {
+//        Guid userId = Guid.NewGuid();
+//        UserModel user = new() { ID = userId };
 
-        CommerceAccountReference account = user.ToCommerceAccount();
+//        CommerceAccountReference account = user.ToCommerceAccount();
 
-        Assert.Equal(userId, account.UserId);
-        Assert.Null(account.OrganizationId);
-    }
+//        Assert.Equal(userId, account.UserId);
+//        Assert.Null(account.OrganizationId);
+//    }
 
-    [Fact]
-    public void ToCommerceOrganizationAccount_maps_organization_identifier()
-    {
-        Guid organizationId = Guid.NewGuid();
+//    [Fact]
+//    public void ToCommerceOrganizationAccount_maps_organization_identifier()
+//    {
+//        Guid organizationId = Guid.NewGuid();
 
-        CommerceAccountReference account = CloudLoginCommerceAccountExtensions.ToCommerceOrganizationAccount(organizationId);
+//        CommerceAccountReference account = CloudLoginCommerceAccountExtensions.ToCommerceOrganizationAccount(organizationId);
 
-        Assert.Equal(organizationId, account.OrganizationId);
-        Assert.Null(account.UserId);
-    }
-}
+//        Assert.Equal(organizationId, account.OrganizationId);
+//        Assert.Null(account.UserId);
+//    }
+//}
--- CloudCommerce/CloudLoginIntegration.cs
+++ CloudCommerce/CloudLoginIntegration.cs
@@ -1,10 +1,10 @@
-using AngryMonkey.CloudLogin;
+//using AngryMonkey.CloudLogin;
 
-namespace AngryMonkey.CloudCommerce;
+//namespace AngryMonkey.CloudCommerce;
 
-public static class CloudLoginCommerceAccountExtensions
-{
-    public static CommerceAccountReference ToCommerceAccount(this UserModel user) => new(UserId: user.ID);
+//public static class CloudLoginCommerceAccountExtensions
+//{
+//    public static CommerceAccountReference ToCommerceAccount(this UserModel user) => new(UserId: user.Id);
 
-    public static CommerceAccountReference ToCommerceOrganizationAccount(Guid organizationId) => new(OrganizationId: organizationId);
-}
+//    public static CommerceAccountReference ToCommerceOrganizationAccount(Guid organizationId) => new(OrganizationId: organizationId);
+//}
